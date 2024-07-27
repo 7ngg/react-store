2438f652dddbf043f48c4c80e29c5cfc1e0049df
Фарманов Магомед Альбертович <Farma_gi04@itstep.edu.az>
2024-07-27 20:10:22 +0400

    feat: title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import Button from "../components/button";
 import Navbar from "../components/navbar";
+import { Carousel } from "react-responsive-carousel";
 
 const Home = () => {
   return (
@@ -6,6 +8,19 @@ const Home = () => {
       <header>
         <Navbar />
       </header>
+      <main>
+        <div className="h-[700px] bg-stone-300 flex flex-col items-center justify-center gap-2">
+          <h1 className="font-bold text-6xl">Title</h1>
+          <h2 className="text-2xl">Subtitle</h2>
+          <div className="flex gap-2">
+            <Button text="Button 1" className="" />
+            <Button
+              text="Button 2"
+              className="bg-black text-white hover:bg-transparent hover:text-black"
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
