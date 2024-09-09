7c51a6521107a36ca7b70a7b51da098955da8c57
tng <m.farm4nov@gmail.com>
2024-09-09 18:48:34 +0400

    constant image height

--- src/components/itemCard.tsx
+++ src/components/itemCard.tsx
@@ -11,7 +11,7 @@ const ItemCard = ({ item }: ItemCardProps) => {
   const dispatch = useAppDispatch();
 
   return (
-    <div className="p-2 border border-stone-400 shadow hover:scale-[103%] w-[300px] flex flex-col justify-between gap-4 rounded duration-200 hover:shadow-xl">
+    <div className="p-2 border border-stone-400 shadow hover:scale-[103%] w-[300px] h-[400px] flex flex-col justify-between gap-4 rounded duration-200 hover:shadow-xl">
       <div className="h-3/5 w-full">
         <img
           className="w-full h-full rounded object-cover"
